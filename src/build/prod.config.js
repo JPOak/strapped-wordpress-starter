07ece47a4c1c94fba10b68404201b98428395001
JP Mac <josh@whosjosh.com>
2019-08-21 13:48:03 -0700

    Change to terser-webpack-plugin for better JS support.

--- src/build/prod.config.js
+++ src/build/prod.config.js
@@ -1,7 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
@@ -28,7 +28,7 @@ module.exports = merge(commonConfig, {
 	devtool: false,
 	optimization: {
 		minimizer: [
-		new UglifyJsPlugin({
+		new TerserPlugin({
 			parallel: true,
 			extractComments: true,
 			sourceMap: false
