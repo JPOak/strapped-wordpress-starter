c7e1718f5f60e74e49d83a9874e14178bd63dc5d
Andrea Musso <eandram@hotmail.it>
2020-01-24 10:45:51 +0000

    Update to setup.php

--- library/setup.php
+++ library/setup.php
@@ -130,3 +130,70 @@ function add_slug_body_class( $classes ) {
 	return $classes;
   }
   add_filter( 'body_class', 'add_slug_body_class' );
+
+/*==================================================================================
+Return svg from ACF file field
+==================================================================================*/
+
+function acfFile_toSvg($file){
+	if($file)
+	return str_replace('\'', '',  var_export(file_get_contents($file), true));
+	
+	/*==================================================================================
+Deregister guttenberg style
+==================================================================================*/
+function wps_deregister_styles() {
+	wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+
+}
+
+/*==================================================================================
+Register color paette for guttenberg
+==================================================================================*/
+function ea_setup() {
+	// Disable Custom Colors
+	add_theme_support( 'disable-custom-colors' );
+  
+	// Editor Color Palette
+	add_theme_support( 'editor-color-palette', array(
+		array(
+			'name'  => __( 'Blue', 'ea-starter' ),
+			'slug'  => 'blue',
+			'color'	=> '#165F97',
+		),
+		array(
+			'name'  => __( 'Orange', 'ea-starter' ),
+			'slug'  => 'orange',
+			'color' => '#F7931C',
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'ea_setup' );
+
+/*==================================================================================
+Allow certain block on Guttenberg
+==================================================================================*/
+ 
+/* function misha_allowed_block_types( $allowed_blocks ) {
+ 
+	return array(
+		'acf/fd-greybgtext',
+		'acf/fd-sharetitle',
+		'acf/fd-bluebg',
+		'acf/fd-button',
+		'acf/fd-teamcard',
+		'acf/fd-imagetext',
+		'acf/mediatextareablock',
+		'core/image',
+		'core/separator',
+		'core/spacer',
+		'core/paragraph',
+		'core/heading',
+		'core/list'
+	);
+ 
+}
+
+add_filter( 'allowed_block_types', 'misha_allowed_block_types' );*/
